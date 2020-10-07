08c1b9495872bfce1a22e826bfa7c1ed920c808b
Arnaud BODEL <contact@osw3.net>
2020-10-07 11:27:01 +0200

    -

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -10,6 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class NoteController extends AbstractController
 {
+    // Liste des notes          /notes
+    // Creation d'une note      /note
+    // Lire une note            /note/{id}
+    // MAJ d'une note           /note/{id}/edit
+    // Supprimer d'une note     /note/{id}/delete
+
     /**
      * @Route("s", name=":index")
      * 
@@ -23,5 +29,35 @@ class NoteController extends AbstractController
         ]);
     }
 
-    
+    /**
+     * @Route("", name=":create")
+     */
+    public function create()
+    {
+        return $this->render('note/create.html.twig');
+    }
+
+    /**
+     * @Route("/{id}", name=":read")
+     */
+    public function read()
+    {
+        return $this->render('note/read.html.twig');
+    }
+
+    /**
+     * @Route("/{id}/edit", name=":update")
+     */
+    public function update()
+    {
+        return $this->render('note/update.html.twig');
+    }
+
+    /**
+     * @Route("/{id}/delete", name=":delete")
+     */
+    public function delete()
+    {
+        return $this->render('note/delete.html.twig');
+    }
 }
