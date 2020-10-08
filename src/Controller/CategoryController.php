5830c705b7fd81fdd0c675b25677f4495703cde2
HENG Kaniha <kaniha@macbook-pro.home>
2020-10-08 11:31:34 +0200

    m

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
-use App\Form\Category;
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,49 +17,65 @@ class CategoryController extends AbstractController
     public function index(CategoryRepository $categoryRepository)
     {
         $categories = $categoryRepository->findAll();
+
         return $this->render('category/index.html.twig', [
             'categories' => $categories
         ]);
     }
+    
     /**
      * @Route("/category", name="category:create")
      */
     public function create(Request $request)
     {
-        
+        // Create new entity
         $category = new Category;
-        $form = $this->createForm(CategoryType::class, $category);
 
+        $form = $this->createForm(CategoryType::class, $category);
         
+        $form->handleRequest($request);
+
+        if ( $form->isSubmitted() ) 
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category); 
+            $em->flush();
+
+            return $this->redirectToRoute("category:index");
+        }
+
+        $form = $form->createView();
+
+        return $this->render('category/create.html.twig', [
+            'form' => $form
+        ]);
     }
+    
     /**
-     * @Route("/category/{id}/read", name="category:read")
+     * @Route("/category/{id}", name="category:read")
      */
     public function read(Category $category)
     {
         return $this->render('category/read.html.twig', [
-            'categories' => $category
+            'category' => $category
         ]);
     }
+    
     /**
-     * @Route("/category/{id}/edit", name="category:upadte")
+     * @Route("/category/{id}/edit", name="category:update")
      */
-    public function upadate(Category $category)
+    public function update(Category $category)
     {
-        $form = $this->createForm(CategoryType::class, $category);
-        $form->handle($request);
-
         return $this->render('category/update.html.twig', [
-           'form' => $form
         ]);
     }
+    
     /**
      * @Route("/category/{id}/delete", name="category:delete")
      */
     public function delete()
     {
         return $this->render('category/delete.html.twig', [
-            
         ]);
     }
 }
