30799dbe019c653fabaca116b5f4cda4c8e42bb1
Arnaud BODEL <contact@osw3.net>
2020-10-08 11:54:18 +0200

    -

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/categories", name="category:index")
+     * @Route("/categories", name="category:index", methods={"HEAD","GET"})
      */
     public function index(CategoryRepository $categoryRepository)
     {
@@ -24,7 +24,7 @@ class CategoryController extends AbstractController
     }
     
     /**
-     * @Route("/category", name="category:create")
+     * @Route("/category", name="category:create", methods={"HEAD","GET","POST"})
      */
     public function create(Request $request)
     {
@@ -52,7 +52,7 @@ class CategoryController extends AbstractController
     }
     
     /**
-     * @Route("/category/{id}", name="category:read")
+     * @Route("/category/{id}", name="category:read", methods={"HEAD","GET"})
      */
     public function read(Category $category)
     {
@@ -62,20 +62,46 @@ class CategoryController extends AbstractController
     }
     
     /**
-     * @Route("/category/{id}/edit", name="category:update")
+     * @Route("/category/{id}/edit", name="category:update", methods={"HEAD","GET","POST"})
      */
-    public function update(Category $category)
+    public function update(Category $category, Request $request)
     {
+        $form = $this->createForm(CategoryType::class, $category);
+        
+        $form->handleRequest($request);
+
+        if ( $form->isSubmitted() ) 
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category); 
+            $em->flush();
+
+            return $this->redirectToRoute("category:index");
+        }
+
+        $form = $form->createView();
+
         return $this->render('category/update.html.twig', [
+            'form' => $form
         ]);
     }
     
     /**
-     * @Route("/category/{id}/delete", name="category:delete")
+     * @Route("/category/{id}/delete", name="category:delete", methods={"HEAD","GET","DELETE"})
      */
-    public function delete()
+    public function delete(Category $category, Request $request)
     {
+        if ($request->getMethod() == 'DELETE')
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($category); // PAS DE PERSIST
+            $em->flush();
+
+            return $this->redirectToRoute('category:index');
+        }
+
         return $this->render('category/delete.html.twig', [
+            'category' => $category
         ]);
     }
 }
